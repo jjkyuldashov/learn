174b6d1b276246f7e938ae6f43a43abe69088d9b
jjkyuldashov <99941470+jjkyuldashov@users.noreply.github.com>
2022-03-14 11:14:40 +0500

    push

--- hulk.go
+++ hulk.go
@@ -243,8 +243,8 @@ func httpcall(url string, host string, data string, headers arrayFlags, s chan u
 		q.Header.Set("Connection", "keep-alive")
 		q.Header.Set("Host", host)
 		q.Header.Set("Referer", "https://student.adu.uz/education/subjects")
-		q.Header.Set("X-CSRF-Token", "fju31d-3p4iD_FAOk9ecY6K9SqaKX3wREdF5r2LBCLgMY_-Btf2f2vfIYGLZjakJ6c8unroTO35flBXtOuxL9A==")
-		q.Header.Set("Cookie", "_ga=GA1.1.1371172649.1645511335; week=0fc595e1389827a925f24d27ecd1781212dbe7705be86e7df85363953a21a009a%3A2%3A%7Bi%3A0%3Bs%3A4%3A%22week%22%3Bi%3A1%3Bs%3A5%3A%2276256%22%3B%7D; _ga_31T1HFVMCX=GS1.1.1646916759.5.0.1646916759.0; frontend=c54ehj2qubgmevtd7mlu38n9mq; _frontendUser=28265cc7bdc04fe769905cdb11d5835f2350047cef4702fb31cb443894ecbac4a%3A2%3A%7Bi%3A0%3Bs%3A13%3A%22_frontendUser%22%3Bi%3A1%3Bs%3A48%3A%22%5B%223744%22%2C%22vn-iY_LVeK0nilycrHARhSchEqKp1irL%22%2C3600%5D%22%3B%7D; _csrf-frontend=61e5c02e52d3a2c9f7704c2c4621e2bbe6ee80cf4b6c2c525befa55697b63b86a%3A2%3A%7Bi%3A0%3Bs%3A14%3A%22_csrf-frontend%22%3Bi%3A1%3Bs%3A32%3A%224HamO3T3VY1QcTCULKt0aBVrxEs9sxLV%22%3B%7D")
+		// q.Header.Set("X-CSRF-Token", "fju31d-3p4iD_FAOk9ecY6K9SqaKX3wREdF5r2LBCLgMY_-Btf2f2vfIYGLZjakJ6c8unroTO35flBXtOuxL9A==")
+		q.Header.Set("Cookie", "_ga=GA1.1.1371172649.1645511335; week=0fc595e1389827a925f24d27ecd1781212dbe7705be86e7df85363953a21a009a%3A2%3A%7Bi%3A0%3Bs%3A4%3A%22week%22%3Bi%3A1%3Bs%3A5%3A%2276256%22%3B%7D; _ga_31T1HFVMCX=GS1.1.1646916759.5.0.1646916759.0; frontend=7i9u3vvtj12kvhann3s4nk27ln; _frontendUser=d4bed1f862d1935870ade4b792f53193151ac3e716cb4eadb5fe223cefca1ed2a%3A2%3A%7Bi%3A0%3Bs%3A13%3A%22_frontendUser%22%3Bi%3A1%3Bs%3A48%3A%22%5B%223744%22%2C%22vn-iY_LVeK0nilycrHARhSchEqKp1irL%22%2C3600%5D%22%3B%7D; _csrf-frontend=814c9d76af0625c23ddf22b4a06c9fd181df37c6f421135f193f2e6d551dd0d3a%3A2%3A%7Bi%3A0%3Bs%3A14%3A%22_csrf-frontend%22%3Bi%3A1%3Bs%3A32%3A%226KzU7n15jBWowWepYlGxQKWwI1_D1BTI%22%3B%7D")
 
 		// Overwrite headers with parameters
 
